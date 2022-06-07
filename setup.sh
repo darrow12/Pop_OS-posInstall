d8fae3369120a9ea5c4b1456178771545265a20b
Kim <nicedarrow12@gmail.com>
2022-06-06 22:19:09 -0300

    📌 feat: definitive version
    + More folders
    + Cool texts
    + Yarn added
    + Only English version
    + More fun commands

--- setup.sh
+++ setup.sh
@@ -3,51 +3,104 @@
 # ------ Variables ------ #
 
 APPS_TO_INSTALL=(
+  vim
+  git
+  curl
+  snapd
   flameshot
   gnome-tweaks
-  neofetch
   cheese
   timeshift
-  snapd
-  git
-  vim
   audacity
+  neofetch
   cmatrix
-  curl
+  yes
+  cowsay
 )
 
-URL_VIVALDI="https://downloads.vivaldi.com/stable/vivaldi-stable_5.1.2567.66-1_amd64.deb"
+URL_VIVALDI="https://downloads.vivaldi.com/stable/vivaldi-stable_5.3.2679.38-1_amd64.deb"
 URL_DISCORD="https://dl.discordapp.net/apps/linux/0.0.17/discord-0.0.17.deb"
-URL_HYPER_TERMINAL="https://github.com/vercel/hyper/releases/download/v3.2.0/hyper_3.2.0_amd64.deb"
-URL_4K_VIDEO_DOWNLOADER="https://dl.4kdownload.com/app/4kvideodownloader_4.20.1-1_amd64.deb"
-URL_TICKTICK="https://appest-public.s3.amazonaws.com/download/linux/linux_deb_x64/ticktick-0.0.5-amd64.deb"
-URL_MEGASYNC="https://mega.nz/linux/repo/xUbuntu_21.10/amd64/megasync-xUbuntu_21.10_amd64.deb"
-URL_VSCODE="https://az764295.vo.msecnd.net/stable/c722ca6c7eed3d7987c0d5c3df5c45f6b15e77d1/code_1.65.2-1646927742_amd64.deb"
+URL_HYPER_TERMINAL="https://github.com/vercel/hyper/releases/download/v3.2.3/hyper_3.2.3_amd64.deb"
+URL_4K_VIDEO_DOWNLOADER="https://dl.4kdownload.com/app/4kvideodownloader_4.20.4-1_amd64.deb"
+URL_TICKTICK="https://appest-public.s3.amazonaws.com/download/linux/linux_deb_x64/ticktick-1.0.11-amd64.deb"
+URL_MEGASYNC="https://mega.nz/linux/repo/xUbuntu_22.04/amd64/megasync-xUbuntu_22.04_amd64.deb"
+URL_VSCODE="https://az764295.vo.msecnd.net/stable/c3511e6c69bb39013c4a4b7b9566ec1ca73fc4d5/code_1.67.2-1652812855_amd64.deb"
+
 
 DOWNLOADS_DIRECTORY="$HOME/Downloads/softwares"
 
-# ------------ #
+# ------ When the script begins ------ #
 
-### Atualizando a parada toda
-sudo apt update
-sudo apt upgrade -y
-sudo apt dist-upgrade -y
-sudo apt full-upgrade -y
-sudo apt install -f
-sudo apt update -y
-flatpak update -y
+clear
+echo "P)ppppp                           O)oooo   S)ssss "
+echo "P)    pp                         O)    oo S)    ss"
+echo "P)ppppp   o)OOO  p)PPPP          O)    oo  S)ss   "
+echo "P)       o)   OO p)   PP         O)    oo      S) "
+echo "P)       o)   OO p)   PP         O)    oo S)    ss"
+echo "P)        o)OOO  p)PPPP  #######  O)oooo   S)ssss "
+echo "                 p)                               "
+echo "                 p)                               "
+echo "                                ##                   t)           l)L  l)L  "
+echo "                                                   t)tTTT          l)   l)  "
+echo "p)PPPP   o)OOO   s)SSSS         i) n)NNNN   s)SSSS   t)   a)AAAA   l)   l)  "
+echo "p)   PP o)   OO s)SSSS  ####### i) n)   NN s)SSSS    t)    a)AAA   l)   l)  "
+echo "p)   PP o)   OO      s)         i) n)   NN      s)   t)   a)   A   l)   l)  "
+echo "p)PPPP   o)OOO  s)SSSS          i) n)   NN s)SSSS    t)T   a)AAAA l)LL l)LL "
+echo "p)                                                                          "
+echo "p)                                                                              by: Darrow"
+echo ""
+echo ""
+echo "Take a sit, wait, and be prepared to press y sometimes."
+echo "Have fun with your coffee :)"
+echo ""
 
-### Criação de algumas pastas
+# ------ Folder structure ------ #
+echo "[📁 Making the folder structure]"
+echo ""
+
 mkdir .themes
 mkdir .icons
+mkdir .webapps
 mkdir code
-cd Pictures/ && mkdir Screenshots
+cd Pictures/
+mkdir Screenshots
 mkdir Storage
+mkdir Memes
 mkdir Wallpapers
+cd ..
+cd Videos/
 mkdir Memes
 cd ..
 
-### Download e instalação dos programas externos
+# ------ Installing APT packages ------ #
+echo "[📦 Installing APT packages]"
+echo ""
+
+for app_name in ${APPS_TO_INSTALL[@]}; do
+  if ! dpkg -i | grep -q $app_name; then
+    sudo apt install -y "$app_name"
+  else
+    echo "[$app_name installed]"
+  fi
+done
+
+sudo apt update
+sudo apt install -f
+
+# ------ Installing some packages ------ #
+echo "[📦 Installing others packages]"
+echo ""
+
+npm i sass -g
+npm install -g nativefier
+snap install asciiquarium
+sudo apt update
+sudo apt install -f
+
+# ------ Installing softwares ------ #
+echo "[📥 Downloading and installing external software]"
+echo ""
+
 mkdir "$DOWNLOADS_DIRECTORY"
 wget -c "$URL_VIVALDI" -P "$DOWNLOADS_DIRECTORY"
 wget -c "$URL_DISCORD" -P "$DOWNLOADS_DIRECTORY"
@@ -60,29 +113,10 @@ wget -c "$URL_VSCODE" -P "$DOWNLOADS_DIRECTORY"
 sudo dpkg -i $DOWNLOADS_DIRECTORY/*.deb
 sudo apt install -f
 
-### Instação dos programas no APT
-for app_name in ${APPS_TO_INSTALL[@]}; do
-  if ! dpkg -i | grep -q $app_name; then
-    sudo apt install -y "$app_name"
-  else
-    echo "[Programa instalado] - $app_name"
-  fi
-done
+# ------ Installing Flatpak packages ------ #
+echo "[📦 Installing Flatpak packages]"
+echo ""
 
-sudo apt update
-sudo apt install -f
-# sudo apt-get install ubuntu-restricted-extras
-
-### Instalação do Node
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-source ~/.bashrc
-sudo update
-nvm list-remote
-nvm install v16.14.2
-
-### 
-
-### Instalando pacotes Flatpak
 flatpak install figma -y
 flatpak install spotify -y
 flatpak install slack -y
@@ -92,7 +126,36 @@ flatpak install flathub org.gimp.GIMP -y
 flatpak install flathub org.videolan.VLC -y
 flatpak install flathub org.onlyoffice.desktopeditors -y
 
-# ------ Última atualização ------ #
+# ------ Installing Node ------ #
+echo "[📌 Installing Node.js]"
+echo ""
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+source ~/.bashrc
+nvm install v16.15.1
+
+# ------ Installing Yarn ------ #
+echo "[📦 Installing Yarn]"
+echo ""
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update && sudo apt install --no-install-recommends yarn
+export PATH="~/.bashrc:`yarn global bin`"
+
+# ------ CapsLock delay fixer ------ #
+echo "[📝 Fixing capslock delay]"
+echo ""
+
+git clone https://github.com/hexvalid/Linux-CapsLock-Delay-Fixer.git
+cd Linux-CapsLock-Delay-Fixer/
+mv bootstrap.sh ..
+cd ..
+bash -ic "sh bootstrap.sh"
+rm -r Linux-CapsLock-Delay-Fixer/
+
+# ------ Updating all ------ #
+echo "[⚡ Almost in the end! Updating all the things]"
+echo ""
 
 sudo apt update
 sudo apt upgrade
@@ -102,8 +165,17 @@ sudo apt autoclean
 sudo apt install -f
 flatpak update
 
-echo "Instalação concluída!"
+# ------ Installation completed ------ #
+
+clear
+neofetch
+echo "📁 Your archives ⤵"
 echo ""
-echo "Caso não consiga utilizar um programa, feche e abra o terminal, ou, reinicie o computador com: sudo reboot now"
-echo "Caso nenhum dessas dicas funcione, foi mal, e, tente instalar o programa manualmente."
-# sudo reboot now
+ls
+echo ""
+echo "✅ Installation completed!"
+echo "Enjoy your new computer! 💻"
+echo ""
+echo "💬 If you have any questions, please contact me on Discord: Darrow#9826"
+echo ""
+cowsay Have fun!
